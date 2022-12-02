46dd4f0a4b90f3a92167a47974e6b6919b6ae1a6
Ricky12Awesome <29931568+Ricky12Awesome@users.noreply.github.com>
2022-12-01 23:53:30 -0600

    Day 2 (Super messy)

---
+++ src/days/day02.rs
@@ -0,0 +1,91 @@
+// day!(Day02, Some(13924), Some(209603));
+day!(Day02);
+
+#[repr(u32)]
+#[derive(Clone, Copy, Eq, PartialEq, Ord, PartialOrd)]
+enum Play {
+  Rock = 1,
+  Paper = 2,
+  Scissors = 3,
+}
+
+impl Play {
+  fn from_str(str: &str) -> Self {
+    match str {
+      "A" | "X" => Self::Rock,
+      "B" | "Y" => Self::Paper,
+      "C" | "Z" => Self::Scissors,
+      _ => unreachable!(),
+    }
+  }
+
+  fn from((play, response): (&str, &str)) -> (Self, Self) {
+    (Self::from_str(play), Self::from_str(response))
+  }
+
+  fn from_p2((play, response): (&str, &str)) -> (Self, Self) {
+    let play = Self::from_str(play);
+
+    match response {
+      "X" => (
+        play,
+        match play {
+          Play::Rock => Self::Scissors,
+          Play::Paper => Self::Rock,
+          Play::Scissors => Self::Paper,
+        },
+      ),
+      "Y" => (play, play),
+      "Z" => (
+        play,
+        match play {
+          Play::Rock => Self::Paper,
+          Play::Paper => Self::Scissors,
+          Play::Scissors => Self::Rock,
+        },
+      ),
+      _ => unreachable!(),
+    }
+  }
+
+  fn sum((play, response): (Self, Self)) -> u32 {
+    let n = match (response, play) {
+      (Play::Rock, Play::Scissors) => 6,
+      (Play::Paper, Play::Rock) => 6,
+      (Play::Scissors, Play::Paper) => 6,
+      (a, b) if a == b => 3,
+      _ => 0,
+    };
+
+    response as u32 + n
+  }
+}
+
+impl Day02 {
+  pub fn day(part: Part) -> Answer<u32> {
+    let input = Self::INPUT
+      .lines()
+      .map(|line| line.split_once(' ').unwrap())
+      .collect_vec();
+
+    let part1 = || {
+      input
+        .iter()
+        .copied()
+        .map(Play::from)
+        .map(Play::sum)
+        .sum::<u32>()
+    };
+
+    let part2 = || {
+      input
+        .iter()
+        .copied()
+        .map(Play::from_p2)
+        .map(Play::sum)
+        .sum::<u32>()
+    };
+
+    answer!(part, part1, part2)
+  }
+}
