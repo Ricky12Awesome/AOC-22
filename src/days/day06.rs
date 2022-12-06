f6fc2af40166ed9d1941c323fe7268e90bbf1a5f
Ricky12Awesome <29931568+Ricky12Awesome@users.noreply.github.com>
2022-12-06 00:41:13 -0600

    Day 6 (was really easy)

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,10 +1,23 @@
-// day!(Day06, Some(0), Some(0));
-day!(Day06);
+day!(Day06, Some(1623), Some(3774));
+// day!(Day06);
 
 impl Day06 {
-  pub fn day(part: Part) -> Answer<u32> {
-    let part1 = || 0;
-    let part2 = || 0;
+  pub fn day(part: Part) -> Answer<usize> {
+    let part1 = || {
+      4 + Self::INPUT
+        .as_bytes()
+        .windows(4)
+        .position(|bytes| bytes.iter().all_unique())
+        .unwrap()
+    };
+
+    let part2 = || {
+      14 + Self::INPUT
+        .as_bytes()
+        .windows(14)
+        .position(|bytes| bytes.iter().all_unique())
+        .unwrap()
+    };
 
     answer!(part, part1, part2)
   }
