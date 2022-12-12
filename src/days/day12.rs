d74211953a483b6ad997dfcd0c76edb98e505b68
Ricky12Awesome <29931568+Ricky12Awesome@users.noreply.github.com>
2022-12-12 08:22:12 -0600

    "Optimized"

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,63 +1,89 @@
-day!(12, None, None, |part, input| -> usize {
-  let value = _solve(input, true);
-
-  answer!(part, || value, || 0)
+#[rustfmt::skip]
+day!(12, Some(394), Some(388), |part, input| -> usize {
+  answer!(part, || Day::solve(input, false), || Day::solve(input, true))
 });
 
-fn _solve(input: &str, start_anywhere: bool) -> usize {
-  let height_map = input.lines().map(|line| line.chars());
-  let mut map = HashMap::new();
-  let mut start = Vec::new();
-  let mut end = (0, 0);
+#[derive(Debug, Default)]
+struct Day {
+  map: HashMap<(i32, i32), i32>,
+  start: (i32, i32),
+  end: (i32, i32),
+  anywhere: bool,
+}
 
-  for (y, row) in height_map.enumerate() {
-    for (x, height) in row.enumerate() {
-      let (x, y) = (x as i32, y as i32);
-
-      let value = match height {
-        'S' => {
-          start.push((x, y));
-          b'a'
-        }
-        'a' if start_anywhere => {
-          start.push((x, y));
-          b'a'
-        }
-        'E' => {
-          end = (x, y);
-          b'z'
-        }
-        _ => height as u8,
-      };
-
-      map.insert((x, y), (value - b'a') as i64);
+impl Day {
+  fn new(anywhere: bool) -> Self {
+    Self {
+      anywhere,
+      ..Self::default()
     }
   }
 
-  start
-    .into_iter()
-    .map(|start| find_shortest(&map, start, end))
-    .filter(|&it| it > 0)
-    .min()
-    .unwrap()
-}
+  fn fold_iteration(mut self, ((x, y), c): ((usize, usize), char)) -> Self {
+    let (x, y) = (x as i32, y as i32);
 
-fn find_shortest(map: &HashMap<(i32, i32), i64>, start: (i32, i32), end: (i32, i32)) -> usize {
-  pathfinding::directed::bfs::bfs(
-    &start,
-    |&(x, y)| {
-      [(x, y + 1), (x, y - 1), (x - 1, y), (x + 1, y)]
-        .into_iter()
-        .filter_map(|p| Some((p, *map.get(&p)?)))
-        .filter(|&(_, h)| h == map[&(x, y)] || h == map[&(x, y)] + 1 || h <= map[&(x, y)])
-        .map(|(p, _)| p)
-        .collect_vec() // needs to be collected because stupid lifetimes
-        .into_iter()
-    },
-    |&(x, y)| (x, y) == end,
-  )
-  .as_ref()
-  .map(Vec::len)
-  .unwrap_or(1)
-    - 1
+    match c {
+      'S' if self.anywhere => self.end = (x, y),
+      'E' if self.anywhere => self.start = (x, y),
+      'S' => self.start = (x, y),
+      'E' => self.end = (x, y),
+      _ => (),
+    }
+
+    let c = match c {
+      'S' => 'a',
+      'E' => 'z',
+      c => c,
+    };
+
+    self.map.insert((x, y), c as i32 - 'a' as i32);
+    self
+  }
+
+  fn solve(input: &str, anywhere: bool) -> usize {
+    let day = input
+      .lines()
+      .enumerate()
+      .map(|(y, line)| (y, line.chars().enumerate()))
+      .flat_map(|(y, line)| line.map(move |(x, c)| ((x, y), c)))
+      .fold(Self::new(anywhere), Self::fold_iteration);
+
+    day.find_shortest()
+  }
+
+  fn check_end(&self, pos: (i32, i32)) -> bool {
+    if self.anywhere {
+      self.map[&pos] == 0
+    } else {
+      pos == self.end
+    }
+  }
+
+  fn check_dst(&self, src: i32, dst: i32) -> bool {
+    if self.anywhere {
+      src == dst || dst == src + 1 || dst <= src
+    } else {
+      src == dst || src == dst + 1 || src <= dst
+    }
+  }
+
+  fn find_shortest(&self) -> usize {
+    pathfinding::directed::bfs::bfs(
+      &self.start,
+      |&(x, y)| {
+        [(x, y + 1), (x, y - 1), (x - 1, y), (x + 1, y)]
+          .into_iter()
+          .filter_map(|p| Some((p, *self.map.get(&p)?)))
+          .filter(|&(_, h)| self.check_dst(h, self.map[&(x, y)]))
+          .map(|(p, _)| p)
+          .collect_vec() // needs to be collected because stupid lifetimes
+          .into_iter()
+      },
+      |&pos| self.check_end(pos),
+    )
+    .as_ref()
+    .map(Vec::len)
+    .map(|len| len - 1)
+    .unwrap_or(0)
+  }
 }
