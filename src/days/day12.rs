bcde8ca5fcc8b63af5ebdeae13f5010af71f53a5
Ricky12Awesome <29931568+Ricky12Awesome@users.noreply.github.com>
2022-12-12 02:14:10 -0600

    Day 12 Part 1

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,3 +1,52 @@
-day!(12, None, None, |part, _input| -> u64 {
+day!(12, None, None, |part, input| -> u64 {
+  _solve(input);
+
   answer!(part, || 0, || 0)
 });
+
+fn _solve(input: &str) {
+  let height_map = input.lines().map(|line| line.chars());
+  let mut map = HashMap::new();
+  let mut starting = (0, 0);
+  let mut end = (0, 0);
+
+  for (y, row) in height_map.enumerate() {
+    for (x, height) in row.enumerate() {
+      let (x, y) = (x as i32, y as i32);
+
+      let value = match height {
+        'S' => {
+          starting = (x, y);
+          b'a'
+        }
+        'E' => {
+          end = (x, y);
+          b'z'
+        }
+        _ => height as u8,
+      };
+
+      map.insert((x, y), (value - b'a') as i64);
+    }
+  }
+
+  let result = pathfinding::directed::bfs::bfs(
+    &starting,
+    |&(x, y)| {
+      [(x, y + 1), (x, y - 1), (x - 1, y), (x + 1, y)]
+        .into_iter()
+        .filter_map(|p| Some((p, *map.get(&p)?)))
+        .filter(|&(_, h)| h == map[&(x, y)] || h == map[&(x, y)] + 1 || h <= map[&(x, y)])
+        .map(|(p, _)| p)
+        .collect_vec() // needs to be collected because stupid lifetimes
+        .into_iter()
+    },
+    |&(x, y)| (x, y) == end,
+  );
+
+  dbg!(&starting);
+  dbg!(&end);
+  dbg!(&result);
+
+  println!("{}", result.as_ref().map(Vec::<_>::len).unwrap_or(0));
+}
